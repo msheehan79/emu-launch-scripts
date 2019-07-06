0ca9c0e400ac06098308365cc05c84fff93d6579
Mike Sheehan <msheehan79@yahoo.com>
2019-07-06 01:58:08 -0400

    Launch script cleanup.

--- launch/Arcade-Model 2.cmd
+++ launch/Arcade-Model 2.cmd
@@ -1,9 +1,10 @@
 @echo off
 :: Parameters
-:: ROM file
+:: ROM file (full path)
 :: Xpadder Profile 1
 :: Xpadder Profile 2
 :: Emulator Command line params
+:: ROM file (base filename only)
 
 :: Set the current directory
 pushd "%~dp0"
@@ -14,18 +15,10 @@ set config=%~3
 set xpadder_p1=%~4
 set xpadder_p2=%~5
 
-:: Set dummy parameters (Test Data Only)
-:: set rom=Defender (USA).a52
-:: set config=Altirra-relative5.ini
-:: set xpadder_p1=Atari 5200 P1 Default
-:: set xpadder_p2=Atari 5200 P2 Default
-
-::@echo Launch dir: "%~dp0" >>C:\Emulation\launch\output.txt
-::@echo Current dir: "%CD%" >>C:\Emulation\launch\output.txt
-
 :: Model 2 emulator expects the config files to be in the launch directory, so switch to the config file dir
 pushd "..\configs\arcade-model2\"
 
+:: Set the launch strings for Xpadder and Model 2 Emulator
 set xpadder_launch="..\..\xpadder\Xpadder.exe" "..\configs\xpadder\profiles\%xpadder_p1%" "..\configs\xpadder\profiles\%xpadder_p2%"
 set emulator_launch="..\..\emulators\model2\emulator.exe" %rom%
 
