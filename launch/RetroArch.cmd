d3cb3b416026ac02de415d86d1a97bc26471c6c7
Mike Sheehan <msheehan79@yahoo.com>
2019-07-18 00:52:22 -0400

    The MAME customized launch command broke core option and remap file structure so go back to the original method.

--- launch/RetroArch.cmd
+++ launch/RetroArch.cmd
@@ -10,28 +10,17 @@
 pushd "%~dp0"
 
 :: Set parameters
+set "rom=%~1"
 set core=%~2
 set config=%~3
 set xpadder_p1=%~4
 set xpadder_p2=%~5
 
-IF "%core%" EQU "mamearcade_libretro" (
-    set "rom=%~6"
-) ELSE (
-    set "rom=%~1"
-)
-
 :: Set the launch strings for Xpadder and RA
 set xpadder_launch="..\xpadder\Xpadder.exe" "..\configs\xpadder\profiles\%xpadder_p1%" "..\configs\xpadder\profiles\%xpadder_p2%"
-
-IF "%core%" EQU "mamearcade_libretro" (
-    set emulator_launch="..\emulators\retroarch\retroarch.exe" -L "..\emulators\retroarch-cores\%core%.dll" "%rom% -rp ..\roms\arcade\mame;..\roms\arcade\neo-geo;..\bios\arcade-mame;..\bios\arcade-neogeo" -c "..\configs\%config%" -v --log-file "..\emulators\retroarch\retroarch-log.txt"
-) ELSE (
-    set emulator_launch="..\emulators\retroarch\retroarch.exe" -L "..\emulators\retroarch-cores\%core%.dll" "%rom%" -c "..\configs\%config%" -v --log-file "..\emulators\retroarch\retroarch-log.txt"    
-)
+set emulator_launch="..\emulators\retroarch\retroarch.exe" -L "..\emulators\retroarch-cores\%core%.dll" "%rom%" -c "..\configs\%config%" -v --log-file "..\emulators\retroarch\retroarch-log.txt"    
 
 :: Alternate debug launch commands
-:: IMPORTANT cannot put any comments inside the parenthesis above, else it will fail
 ::set emulator_launch="..\emulators\retroarch\retroarch_debug.exe" -L "..\emulators\retroarch-cores\%core%.dll" "%rom%" -c "..\configs\%config%" -v --log-file "..\emulators\retroarch\retroarch-log.txt"
 ::set emulator_launch="..\emulators\retroarch\retroarch.exe" -L "..\emulators\retroarch-cores\%core%.dll" "%rom%" -c "..\configs\%config%"
 
