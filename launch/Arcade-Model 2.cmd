0bdcfdb2af0d4e7d3e8438edb11026ce4e4a0291
Mike Sheehan <msheehan79@yahoo.com>
2019-08-22 01:33:54 -0400

    Update Model 2 and Taito Arcade launch scripts to extract the filename from the full path so the launch string doesn't have to include an exception for these emus.

--- launch/Arcade-Model 2.cmd
+++ launch/Arcade-Model 2.cmd
@@ -1,16 +1,15 @@
 @echo off
 :: Parameters
-:: ROM file (full path)
+:: ROM file (base filename only)
 :: Xpadder Profile 1
 :: Xpadder Profile 2
 :: Emulator Command line params
-:: ROM file (base filename only)
 
 :: Set the current directory
 pushd "%~dp0"
 
 :: Set parameters
-set "rom=%~6"
+set "rom=%~n1"
 set config=%~3
 set xpadder_p1=%~4
 set xpadder_p2=%~5
@@ -18,9 +17,9 @@ set xpadder_p2=%~5
 :: Model 2 emulator expects the config files to be in the launch directory, so switch to the config file dir
 pushd "..\configs\arcade-model2\"
 
-:: Set the launch strings for Xpadder and Model 2 Emulator
+:: Set the launch strings for Xpadder and emulator
 set xpadder_launch="..\..\xpadder\Xpadder.exe" "..\configs\xpadder\profiles\%xpadder_p1%" "..\configs\xpadder\profiles\%xpadder_p2%"
 set emulator_launch="..\..\emulators\model2\emulator.exe" %rom%
 
-:: Launch xpadder profiles, then Launch the emuator with the config file
+:: Launch xpadder profiles, then Launch the emulator with the config file
 start "" %xpadder_launch% && timeout 1 && start "" /WAIT %emulator_launch%
--- launch/Arcade-Taito.cmd
+++ launch/Arcade-Taito.cmd
@@ -1,6 +1,6 @@
 @echo off
 :: Parameters
-:: ROM file
+:: ROM file (Base filename only)
 :: Xpadder Profile 1
 :: Xpadder Profile 2
 :: Emulator Command line params
@@ -8,28 +8,17 @@
 :: Set the current directory
 pushd "%~dp0"
 
-set "rom=%~1"
+:: Set parameters
+set "rom=%~n1"
 set config=%~3
 set xpadder_p1=%~4
 set xpadder_p2=%~5
 
-:: for these arcade games, the filename alone minus extension and path is used as the parameter to the emulator
-FOR /F "delims=" %%i IN ("%rom%") DO (
-    set romname=%%~ni
-)
-
-:: Set parameters
-:: set rom=C:\Emulation\roms\intellivision\Armor Battle (World).int
-:: set config=intellivision\kbdhackfile.kbd
-:: set core=prosystem_libretro
-
-:: set xpadder_p1=Intellivision Left
-:: set xpadder_p2=Intellivision Right
-
+:: Set the launch strings for Xpadder and emulator
 set xpadder_launch="..\xpadder\Xpadder.exe" "..\configs\xpadder\profiles\%xpadder_p1%" "..\configs\xpadder\profiles\%xpadder_p2%"
-set emulator_launch="..\emulators\typextra\typextra.exe" -gl "%romname%" -hc
+set emulator_launch="..\emulators\typextra\typextra.exe" -gl "%rom%" -hc
 
-:: Launch xpadder profiles, then Launch the emuator with the config file
+:: Launch xpadder profiles, then Launch the emulator with the config file
 IF "%xpadder_p1%" NEQ "" (
     start "" %xpadder_launch% && timeout 1 && start "" /WAIT %emulator_launch%
 ) ELSE (
