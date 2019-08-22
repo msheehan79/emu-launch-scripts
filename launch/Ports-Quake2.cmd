88557d1167723812763448dcd9122477fcb27bc1
Mike Sheehan <msheehan79@yahoo.com>
2019-08-22 01:36:50 -0400

    set up launch script for quake 2

---
+++ launch/Ports-Quake2.cmd
@@ -0,0 +1,27 @@
+@echo off
+:: Parameters
+:: ROM file (full path)
+:: Xpadder Profile 1
+:: Xpadder Profile 2
+:: Emulator Command line params
+:: ROM file (base filename only)
+
+:: Set the current directory
+pushd "%~dp0"
+
+:: Set parameters
+set "rom=%~1"
+set config=%~3
+set xpadder_p1=%~4
+set xpadder_p2=%~5
+
+:: Set the launch strings for Xpadder and gzdoom
+set xpadder_launch="..\xpadder\Xpadder.exe" "..\configs\xpadder\profiles\%xpadder_p1%" "..\configs\xpadder\profiles\%xpadder_p2%"
+set emulator_launch="..\emulators\yquake2\yquake2.exe" -datadir ..\roms\ports\quake2 %config% -portable
+
+:: Launch xpadder profiles, then Launch the emulator with the config file
+IF "%xpadder_p1%" NEQ "" (
+    start "" %xpadder_launch% && timeout 1 && start "" /WAIT %emulator_launch%
+) ELSE (
+    start "" /WAIT %emulator_launch%
+)
