9c9b0d7226337add2b24a398b4f9a7560aba35a6
Mike Sheehan <msheehan79@yahoo.com>
2019-08-22 01:39:08 -0400

    set up Duke 3D port

---
+++ launch/Ports-Duke3D.cmd
@@ -0,0 +1,31 @@
+@echo off
+:: Parameters
+:: ROM file (full path)
+:: Xpadder Profile 1
+:: Xpadder Profile 2
+:: Emulator Command line params
+:: ROM file (base filename only)
+
+:: Set the current directory
+pushd "%~dp0"
+
+:: Set parameters
+set "rom=%~nx1"
+set "rompath=%~dp1"
+set config=%~3
+set xpadder_p1=%~4
+set xpadder_p2=%~5
+
+:: eduke32 generates config and cache files in the current directory so switch to the config dir to output them there
+pushd "..\configs\ports\duke3d\"
+
+:: Set the launch strings for Xpadder and emulator
+set xpadder_launch="..\..\..\xpadder\Xpadder.exe" "..\..\xpadder\profiles\%xpadder_p1%" "..\..\xpadder\profiles\%xpadder_p2%"
+set emulator_launch="..\..\..\emulators\eduke32\eduke32.exe" -cfg %config% -j %rompath% -gamegrp %rom%
+
+:: Launch xpadder profiles, then Launch the emulator with the config file
+IF "%xpadder_p1%" NEQ "" (
+    start "" %xpadder_launch% && timeout 1 && start "" /WAIT %emulator_launch%
+) ELSE (
+    start "" /WAIT %emulator_launch%
+)
