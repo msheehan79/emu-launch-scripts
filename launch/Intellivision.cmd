acc5d22e0e7cd27b2fe696885c0334ca514f09c2
Mike Sheehan <msheehan79@yahoo.com>
2019-09-21 10:36:59 -0400

    Adjust JZintv launch script

--- launch/Intellivision.cmd
+++ launch/Intellivision.cmd
@@ -14,7 +14,9 @@ set xpadder_p1=%~4
 set xpadder_p2=%~5
 
 set xpadder_launch="..\xpadder\Xpadder.exe" "..\configs\xpadder\profiles\%xpadder_p1%" "..\configs\xpadder\profiles\%xpadder_p2%"
-set emulator_launch="..\emulators\jzintv\bin\jzintv.exe" -p "..\bios\intellivision" -q -z1440x1080,32 -v1 -f1 %config% "%rom%"
+::set emulator_launch="..\emulators\jzintv\bin\jzintv.exe" -p "..\bios\intellivision" -q -z1440x1080,32 -v1 -f1 %config% "%rom%"
+::set emulator_launch="..\emulators\jzintv\bin\jzintv.exe" -p "..\bios\intellivision" -q -z3 -v1 -f1 %config% "%rom%"
+set emulator_launch="..\emulators\jzintv\bin\jzintv.exe" -p "..\bios\intellivision" -q -z1024x768,32 -v1 -f1 %config% "%rom%"
 
 :: Launch xpadder profiles, then Launch the emulator with the config file
 IF "%xpadder_p1%" NEQ "" (
