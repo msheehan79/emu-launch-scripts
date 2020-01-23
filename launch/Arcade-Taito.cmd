53abd137403a1da0e22fb33f234d842f0069f8d9
Mike Sheehan <msheehan79@yahoo.com>
2020-01-22 22:08:27 -0500

    Add resolution switching to the Taito Type X launch script to set to 1080p

--- launch/Arcade-Taito.cmd
+++ launch/Arcade-Taito.cmd
@@ -14,6 +14,17 @@ set config=%~3
 set xpadder_p1=%~4
 set xpadder_p2=%~5
 
+:: These games don't seem to render properly at 4K so set the display to 1080p
+set base_res="3840x2160, 32 bits @ 60 Hz."
+set target_res="1920x1080, 32 bits @ 60 Hz."
+
+:: Check current resolution and set to 1080p if it is not already
+FOR /F "skip=3 tokens=* USEBACKQ" %%g IN (`..\util\qres /S`) do (
+    IF "%%g" NEQ %target_res% (
+        call :display1080p
+    )
+)
+
 :: Set the launch strings for Xpadder and emulator
 set xpadder_launch="..\xpadder\Xpadder.exe" "..\configs\xpadder\profiles\%xpadder_p1%" "..\configs\xpadder\profiles\%xpadder_p2%"
 set emulator_launch="..\emulators\typextra\typextra.exe" -gl "%rom%" -hc
@@ -24,3 +35,22 @@ IF "%xpadder_p1%" NEQ "" (
 ) ELSE (
     start "" /WAIT %emulator_launch%
 )
+
+:: After game ends, loop through supported resolutions to make sure 4K is supported before attempting to switch back
+FOR /F "tokens=* USEBACKQ" %%g IN (`..\util\qres /L`) do (
+    IF "%%g" EQU %base_res% (
+        call :display4k
+        goto :exit
+    )
+)
+
+:exit
+EXIT /B %ERRORLEVEL%
+
+:display1080p
+..\util\qres /X:1920 /Y:1080 /R:60
+EXIT /B 0
+
+:display4k
+..\util\qres /X:3840 /Y:2160 /R:60
+EXIT /B 0
