5782e8d1ac86a94a015c7845fc8ca9d25db23566
Mike Sheehan <msheehan79@yahoo.com>
2021-05-08 14:05:20 -0400

    Start adding Xenia for Xbox and XBLA

---
+++ launch/Xenia.cmd
@@ -0,0 +1,27 @@
+@echo off
+:: Parameters
+:: ROM file
+:: Xpadder Profile 1
+:: Xpadder Profile 2
+:: Emulator Command line params
+
+:: Set the current directory
+pushd "%~dp0"
+
+set "rom=%~1"
+set config=%~3
+set xpadder_p1=%~4
+set xpadder_p2=%~5
+
+set xpadder_launch="..\xpadder\Xpadder.exe" "..\configs\xpadder\profiles\%xpadder_p1%" "..\configs\xpadder\profiles\%xpadder_p2%"
+set emulator_launch="..\emulators\xenia\xenia.exe" "%rom%" --config="..\configs\%config%"
+
+:: Make sure the emulator remains in focus during startup
+start "" ..\util\emulator-focus Xenia
+
+:: Launch xpadder profiles, then Launch the emulator with the config file
+IF "%xpadder_p1%" NEQ "" (
+    start "" %xpadder_launch% && timeout 1 && start "" /WAIT %emulator_launch%
+) ELSE (
+    start "" /WAIT %emulator_launch%
+)
